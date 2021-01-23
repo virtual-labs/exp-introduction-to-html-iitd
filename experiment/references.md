157e1db90c967ae394c46e56a3f0b92184248843
vlabsdev-iitd <vlabsdev.iitd@gmail.com>
2021-01-23 16:42:46 +0530

    Introduction

--- experiment/references.md
+++ experiment/references.md
@@ -1,1 +1,12 @@
-### Link your references in here
+
+#### References
+
+1. Web Technologies Black Book: HTML, JavaScript, PHP, Java, JSP, XML and AJAX, Kogent Learning Solutions Inc.
+
+2. The Complete Refrence,fifth edithion By- Thomas A.Powell.
+
+3. HTML and CSS: Design and Build Websites By- Jon Duckett.
+
+4. NPTEL: Introduction to HTML NPTEL: HTML-Part-I
+
+5. Learn HTML Online  https://www.learn-html.org/
---
+++ storyboard/mindmap/mindmap.mm
@@ -0,0 +1,123 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Learn HTML" FOLDED="false" ID="ID_1031967823" CREATED="1572441234426" MODIFIED="1572442024892" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.621">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Formatting Tags" POSITION="right" ID="ID_1806987272" CREATED="1572441339398" MODIFIED="1572442013319" HGAP_QUANTITY="60.49999861419205 pt" VSHIFT_QUANTITY="-109.49999673664581 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="&lt;i&gt;" ID="ID_62389409" CREATED="1572441670246" MODIFIED="1572441699789"/>
+<node TEXT="&lt;b&gt;" ID="ID_403350178" CREATED="1572441675186" MODIFIED="1572441704725"/>
+<node TEXT="&lt;u&gt;" ID="ID_1843103575" CREATED="1572441675869" MODIFIED="1572441718526"/>
+<node TEXT="&lt;small&gt;" ID="ID_261272357" CREATED="1572441681605" MODIFIED="1572441724729"/>
+<node TEXT="&lt;big&gt;" ID="ID_398704529" CREATED="1572441674513" MODIFIED="1572441730413"/>
+</node>
+<node TEXT="" POSITION="right" ID="ID_1022502064" CREATED="1572441502699" MODIFIED="1572441502702">
+<edge COLOR="#ff00ff"/>
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Introduction" POSITION="right" ID="ID_376134051" CREATED="1572441484493" MODIFIED="1572442024888" HGAP_QUANTITY="68.7499983683229 pt" VSHIFT_QUANTITY="53.99999839067464 pt">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="" POSITION="right" ID="ID_1622672807" CREATED="1572441502694" MODIFIED="1572441648605">
+<edge COLOR="#00ff00"/>
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Markup" ID="ID_1898053663" CREATED="1572441502703" MODIFIED="1572441646399" HGAP_QUANTITY="36.49999932944777 pt" VSHIFT_QUANTITY="-11.249999664723884 pt"/>
+<node TEXT="Webpages" ID="ID_1812660381" CREATED="1572441512686" MODIFIED="1572441648605" HGAP_QUANTITY="34.999999374151244 pt" VSHIFT_QUANTITY="-1.4999999552965178 pt"/>
+<node TEXT="Text Editors" ID="ID_738263062" CREATED="1572441519989" MODIFIED="1572441632165" HGAP_QUANTITY="32.74999944120647 pt" VSHIFT_QUANTITY="7.499999776482589 pt"/>
+<node TEXT="Browsers" ID="ID_651156797" CREATED="1572441521872" MODIFIED="1572441637831" HGAP_QUANTITY="35.7499993517995 pt" VSHIFT_QUANTITY="7.499999776482589 pt"/>
+</node>
+<node TEXT="Element of Structure" POSITION="left" ID="ID_714365016" CREATED="1572441742918" MODIFIED="1572442016271" HGAP_QUANTITY="64.24999850243334 pt" VSHIFT_QUANTITY="-109.49999673664578 pt">
+<edge COLOR="#00ffff"/>
+<node TEXT="Doctype" ID="ID_1860431268" CREATED="1572441755959" MODIFIED="1572441794170"/>
+<node ID="ID_1869031035" CREATED="1572441773116" MODIFIED="1572441840795"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0">
+      &lt;html&gt;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="&lt;head&gt;" ID="ID_1698245643" CREATED="1572441773810" MODIFIED="1572441847130"/>
+<node TEXT="&lt;body&gt;" ID="ID_838204287" CREATED="1572441774483" MODIFIED="1572441853722"/>
+<node TEXT="Content" ID="ID_952829729" CREATED="1572441778873" MODIFIED="1572441859252"/>
+</node>
+<node TEXT="Heading Tags" POSITION="left" ID="ID_1149540954" CREATED="1572441896585" MODIFIED="1572442020175" HGAP_QUANTITY="69.49999834597116 pt" VSHIFT_QUANTITY="61.49999816715723 pt">
+<edge COLOR="#7c0000"/>
+<node TEXT="&lt;h1&gt;" ID="ID_1584942937" CREATED="1572441912186" MODIFIED="1572441994949" HGAP_QUANTITY="32.74999944120647 pt" VSHIFT_QUANTITY="-4.499999865889553 pt"/>
+<node TEXT="&lt;h2&gt;" ID="ID_194709206" CREATED="1572441918647" MODIFIED="1572441992425" HGAP_QUANTITY="31.99999946355821 pt" VSHIFT_QUANTITY="-0.7499999776482589 pt"/>
+<node TEXT="&lt;h3&gt;" ID="ID_1509890780" CREATED="1572441920482" MODIFIED="1572441989876" HGAP_QUANTITY="31.99999946355821 pt" VSHIFT_QUANTITY="1.4999999552965178 pt"/>
+<node TEXT="&lt;h4&gt;" ID="ID_1136718693" CREATED="1572441921241" MODIFIED="1572441981237" HGAP_QUANTITY="31.249999485909953 pt" VSHIFT_QUANTITY="4.499999865889553 pt"/>
+<node TEXT="&lt;h5&gt;" ID="ID_877912653" CREATED="1572441922562" MODIFIED="1572441984312" HGAP_QUANTITY="30.499999508261695 pt" VSHIFT_QUANTITY="2.2499999329447764 pt"/>
+<node TEXT="&lt;h6&gt;" ID="ID_1849029603" CREATED="1572441923164" MODIFIED="1572441986271" HGAP_QUANTITY="30.499999508261695 pt" VSHIFT_QUANTITY="4.499999865889553 pt"/>
+</node>
+</node>
+</map>
